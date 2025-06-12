cd631c8d844d7bb59b8d1ef3be157149bb2ed6a5
Dmitry Plastinin <uncellon@yandex.ru>
2025-06-12 14:39:18 +0300

    Add smooth chunk loading

--- scripts/chunk_loader.gd
+++ scripts/chunk_loader.gd
@@ -1,36 +1,51 @@
-class_name ChunkLoader extends Resource
+class_name ChunkLoader
+extends RefCounted
 
-var thread = Thread.new()
+@export var chunk_create_strategy: Callable
+@export var workers_count = 8 # How many threads will load chunks
+@export var thread_chunks_chunk_limit = 4 # How many chunks thread will load per loop
+
+var threads: Array[Thread]
 var is_running = true
 var sem = Semaphore.new()
 
 var chunk_positions_to_load = []
-var chunk_positions_to_load_mtx = Mutex.new()
+var chunk_poss_to_load_mtx = Mutex.new()
 
 var loaded_chunks = []
 var loaded_chunks_mtx = Mutex.new()
 
-var chunk_positions_to_load_local = []
-var loaded_chunks_local = []
-
-@export var chunk_create_strategy: Callable
+var chunk_poss_to_load_locals = [] # Array of array
 
 func _init() -> void:
-	thread.start(loop)
+	threads.resize(workers_count)
+	chunk_poss_to_load_locals.resize(workers_count)
+
+	for i in workers_count:
+		chunk_poss_to_load_locals[i] = []
+		threads[i] = Thread.new()
+		threads[i].start(loop.bind(i))
+
+	# !!! For-each in this scenario causes memory leak !!!
+	# for thread in threads:
+	# 	thread = Thread.new()
+	# 	thread.start(loop)
 
 func _exit_tree() -> void:
 	is_running = false
-	sem.post()
-	thread.wait_to_finish()
+	sem.post(workers_count)
+	for thread in threads:
+		thread.wait_to_finish()
 
 func push_chunk_positions_to_load(new_chunk_positions_to_load: Array):
 	loaded_chunks_mtx.lock()
-	chunk_positions_to_load_mtx.lock()
+	chunk_poss_to_load_mtx.lock()
 
 	# Check every chunk position in already prepared and loaded chunks
-	for chunk_position in chunk_positions_to_load_local: # Currently processing queue
-		if new_chunk_positions_to_load.has(chunk_position):
-			new_chunk_positions_to_load.erase(chunk_position)
+	for chunk_poss_to_load_local in chunk_poss_to_load_locals:
+		for chunk_position in chunk_poss_to_load_local: # Currently processing queue
+			if new_chunk_positions_to_load.has(chunk_position):
+				new_chunk_positions_to_load.erase(chunk_position)
 	for chunk_position in chunk_positions_to_load: # Queue to process
 		if new_chunk_positions_to_load.has(chunk_position):
 			new_chunk_positions_to_load.erase(chunk_position)
@@ -40,13 +55,13 @@ func push_chunk_positions_to_load(new_chunk_positions_to_load: Array):
 
 	chunk_positions_to_load.append_array(new_chunk_positions_to_load)
 
-	chunk_positions_to_load_mtx.unlock()
+	chunk_poss_to_load_mtx.unlock()
 	loaded_chunks_mtx.unlock()
-	sem.post()
+	sem.post(workers_count)
 
 func has_loaded_chunks() -> bool:
-	var result = !loaded_chunks.is_empty()
 	loaded_chunks_mtx.lock()
+	var result = !loaded_chunks.is_empty()
 	loaded_chunks_mtx.unlock()
 	return result
 
@@ -57,22 +72,31 @@ func get_loaded_chunks() -> Array:
 	loaded_chunks_mtx.unlock()
 	return tmp
 
-func loop() -> void:
+func loop(index: int) -> void:
+	var loaded_chunks_local = []
+
 	while is_running:
 		sem.wait()
 
 		# Copy "chunk positions to load" to local version to minimize mutex block
-		chunk_positions_to_load_mtx.lock()
-		chunk_positions_to_load_local = chunk_positions_to_load.duplicate()
-		chunk_positions_to_load.clear()
-		chunk_positions_to_load_mtx.unlock()
+		chunk_poss_to_load_mtx.lock()
+
+		var slice_end = thread_chunks_chunk_limit
+		if slice_end > chunk_positions_to_load.size():
+			slice_end = chunk_positions_to_load.size()
+		#print(str("Before: ", chunk_positions_to_load))
+		chunk_poss_to_load_locals[index] = chunk_positions_to_load.slice(0, slice_end)
+		chunk_positions_to_load = chunk_positions_to_load.slice(slice_end)
+		#print(str("Slice: ", chunk_poss_to_load_locals[index]))
+		#print(str("Remains: ", chunk_positions_to_load))
+
+		chunk_poss_to_load_mtx.unlock()
+
+		if chunk_poss_to_load_locals[index].size() == 0:
+			continue
 
 		# Loading
-		for chunk_position_to_load in chunk_positions_to_load_local:
-			#var chunk = Chunk.new()
-			#chunk.chunk_position = chunk_position_to_load
-			#chunk.init()
-			#loaded_chunks_local.append(chunk)
+		for chunk_position_to_load in chunk_poss_to_load_locals[index]:
 			loaded_chunks_local.append(chunk_create_strategy.call(chunk_position_to_load))
 
 		# Copy loaded chunks into output queue
